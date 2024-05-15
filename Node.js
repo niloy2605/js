47faed14987d54c2956452e7474295c168daafa3
Niloy <161234312+niloy2605@users.noreply.github.com>
2024-05-15 12:29:09 +0600

    Add files via upload

---
+++ Node.js
@@ -0,0 +1,433 @@
+
+ 
+// //  
+
+// // let a = 156;
+// // let  b = 96;
+
+// // let c = a % b ;
+
+// // console.log(96**2);
+
+// // let a = 70;
+// // //  a = a + 20;
+// //    a = 40;
+// //  console.log(a);
+
+// // let a = 10;
+// // let b = 10;
+// // let c = 90;
+
+// // if  ((a===b &&  !b===c)){
+// //      console.log( "hello there");
+// // }else{
+// //      console .log("its not okey")
+// // }
+
+// //  let a = document.querySelector("h1");
+
+// //  let result = prompt( "Enter a number")
+
+// //  result = Number(result)
+
+// //  if(result % 2 === 0){
+// //      a.innerHTML = "even number"
+// //     //  console.log("even number")
+// //  }else{
+// //     a.innerHTML = "odd number"
+// //     //  console.log(" its an odd number")
+// //  }
+
+// // let num = prompt ("Enter a number")
+
+// // if (num % 5 === 0){
+// //      console.log(num, ("is a multiple of 5"));
+// // }else{
+// //      console .log (num,("is not multiple of 5"))
+// // }
+
+// // practice question 
+
+// // let a = document.querySelector("h1");
+
+
+// // let c = document.querySelector("h1");
+
+// // let score = prompt("Enter your score");
+// //  score = Number(score);
+// // let grade;
+
+// // if(score>= 90 && score <= 100){
+// //      grade = "  Accodring to your score, your grade was, A";
+// // }else if(score >= 80 && score <= 89){
+// //      grade = "Accodring to your score, your grade was ,  B";
+// // }else if (score >= 70 && score <= 79){
+// //      grade = "Accodring to your score, your grade was ,  C";
+// // }else if (score >= 60 && score <= 69){
+// //      grade = " Accodring to your score, your grade was ,  D";
+// // }else if (score >= 0 && score <= 59){
+// //      grade = " Accodring to your score, your grade was ,  F";
+// // }
+
+// //  c.innerHTML = grade;
+// // console.log("accodring to your score, your grade was : ", grade);
+
+
+// // Arithmethics operatars
+// // let a = 5;
+// // let b = 2;
+// // console .log ("a =",a ,"&b = ",b);
+// // console .log ("a + b =", a + b);
+// // console .log ("a - b =", a - b);
+// // console .log ("a * b =", a * b);
+// // console .log ("a / b =", a / b);
+// // console .log ("a % b =", a % b);
+// // console .log ("a ** b =", a ** b);
+
+// // unery operatars
+
+// // let a = 7;
+// //  let b = 3;
+
+// //  console .log ("a = ", a ,"& b = ",b )
+
+// //  console .log ("--a = ",--a)
+
+// // Assignment operatore
+
+// // let a = 7;
+// //  let b = 3;
+
+// //  a **= 4;
+
+// //  console .log ("a = ", a)
+
+// // Comparison operators
+
+// // let a = 7;
+// // let b = 7;
+
+// //  console .log("a <= b ", a <= b)
+
+// // logical operators
+
+// // let a = 7;
+// // let b = 4;
+
+// //  let con1 = a > b;
+// //  let con2 = a === b;
+
+// //  console .log (" !(b === 4) = ", !(b === 4))
+
+
+// // Conditinol Statement 
+
+// // let  age = 17;
+
+// // if (age >= 18){
+// //       console.log("you can vote");
+// // }
+// // if (age <= 18){
+// //      console.log("you cannot vote");
+// // }
+
+
+// // let mode ="light"
+// // let color;
+
+// // if (mode === "dark"){
+// //     color = "black";
+// // }else {
+// //       color = "white"
+// // }
+
+// // console.log(color);
+
+// let age = 15;
+
+// if (age >= 18){
+//       console .log("You can vote")
+// }else {
+//       console.log("You cannot vote")
+// }
+
+// // Even or odd
+
+// // let num = 13894573994;
+// // if (num % 2 === 0){
+// //      console.log ( num ," is even")
+// // }else{
+// //      console.log ( num," is odd")
+// // }
+
+
+// // // if -else -else if
+
+// // let mode = ''
+// // let color ;
+
+// // if (mode === "dark"){
+// //     color = "purpal"
+// // }else if (mode === "blue"){
+// //       color = "balck"
+// // }else if (mode === "light"){
+// //       color ="white"
+// // }else{
+// //      color = "yellow"
+// // }
+     
+
+// // console.log(color)
+
+// // let age = 15;
+
+// //   let result = age > 18 ? "adult" : "not adult";
+
+// // console.log(result);
+
+// // let name = prompt('enter your name')
+
+// // console.log (name)
+
+// // let number = prompt ("enter a numbe:");
+
+// // if (number % 7 === 0){
+// //      console.log(number,"is multipal of 7")
+// // }else{
+// //      console.log(number,"is not a multipal of 7")
+// // }
+
+
+// // let result = document.querySelector("h1")
+
+// // let score = prompt ("enter your score ");
+// // let grade;
+
+// // if (score >= 90 && score <= 100){
+// //       grade = " Accouting to your score , your grade was ,A";
+// // }else if (score >= 70  && score <= 89){
+// //      grade = " Accouting to your score , your grade was,B";
+// // }else if (score >= 60 && score <= 69){
+// //       grade = "Accouting to your score , your grade was ,C";
+// // }else if (score >= 50  && score <= 59){
+// //       grade = "Accouting to your score , your grade was ,D"
+// // }else if (score >= 0 && score <= 49){
+// //       grade = "F"
+// // }
+
+// // result.innerHTML = grade;
+
+
+// // let age = prompt("What is your age?")
+
+// // if (age >= 10 && age <= 20){
+// //      console.log ("Your age lies between 10 and 20")
+// // }else{
+// //       console.log ("Your age dosen`t lies between 10 and 20")     
+// // }
+
+// // function Name(a , b){
+// //     let first = a;
+// //     let second = b;
+// //     let c = first % second
+// //    console.log(c)
+// // }
+
+// // Name(50 , 60)
+// // Name(500 , 123)
+// // Name(345 , 60)
+// // Name(50 , 670)
+// // Name(9870 , 60)
+
+// // let on = document.querySelector(".on")
+// // let light = document.querySelector(".light")
+// // let off = document.querySelector(".off")
+
+// // on.addEventListener("click" , function(){
+// //      light.src = "https://www.w3schools.com/js/pic_bulbon.gif"
+// // })
+
+// // off.addEventListener("click", function(){
+// //      light.src = `https://www.w3schools.com/js/pic_bulboff.gif`
+// // })
+
+
+// function myFuncation (){
+//       console.log ("Welcome to apna colloge");
+//       console.log ("We are Learning JS")
+// }
+
+// myFuncation();
+
+// // Funcatio 2 number , sum
+
+// function sum(x , y){
+//  s = x + y ;
+//   return s;
+  
+// }
+
+//  let val = sum(48,9);
+//  console.log(val)
+
+
+// //  Arrow fanction
+
+// const arrowsum  = (a, b) => {
+//       console.log(a + b)
+// };
+// // Multiplication arrow function
+
+// function mul (a,b){
+//       return a * b;
+// }
+
+// const arrowmul = (a,b) => {
+//     return (a*b);
+// }
+// const printHello = () => {
+//        console.log("Hey There")
+// }
+
+// function countVowels(str){
+//       let count = 0;
+//      for (const char of str){
+//        if (
+//           char === `a` ||
+//           char === `e` ||
+//           char === `i` ||
+//           char === `o`  || 
+//           char === `u`){
+//              count ++;
+//        }
+//      } 
+//      return count;
+// }
+
+
+// const countVow = (str) => {
+//       let count = 0;
+//       for (const char of str){
+//         if (
+//            char === `a` ||
+//            char === `e` ||
+//            char === `i` ||
+//            char === `o`  || 
+//            char === `u`){
+//               count ++;
+//         }
+//       } 
+//       return count;
+// }
+
+// let arr = ['Johirul', 'Jahan' , 'Niloy'];
+
+// arr.forEach( (val ,idx ,arr) => {
+//       console.log(val.toUpperCase(), idx, arr)
+// })
+
+// // forEach function methods
+// let arro = [2,4,5,9];
+
+// arro.forEach( (val) => {
+//       console.log(val*val)
+// })
+
+
+// // Map function methods
+// let numb = [68,74,86,99];
+
+// let newarr = numb.map( (val) => {
+//    return val * val;  
+// })
+
+// console.log(newarr)
+
+// // Filter function methods 
+
+// let numarr = [1,2,3,4,5,6,7,8,9,10];
+
+//  let evenarr = numarr.filter( (val) => {
+//        return val > 7 ;
+//  });
+
+//  console.log (evenarr);
+
+// // Array Reduce function methods
+
+//  let array = [2,5,7,8,9];
+
+//   const output =array.reduce ((res , curr) => {
+//        return res < curr ? res : curr ;
+//  });
+
+//  console.log(output);
+
+
+//  let marks = [98,90,99,45,67,87];
+
+//  let topper  = marks.filter((val) => {
+//        return val > 60 ;
+//  })
+
+//  console.log(topper)
+
+// // //  let n = prompt("Enter a Number ")
+
+// //  let arra = [];
+
+// //  for (let i = 0; i < n; i++){
+// //        arra [i-1] = i;
+// //  }
+
+// //  console.log(arra)
+
+ 
+//  let on = document.querySelector(".on")
+//  let light = document.querySelector(".light")
+//  let off = document.querySelector(".off")
+
+//  on.addEventListener("click" , function(){
+//      light.src = `https://www.w3schools.com/js/pic_bulbon.gif`
+     
+//  })
+
+
+// off.addEventListener("click",function() {
+//       light.src = `https://www.w3schools.com/js/pic_bulboff.gif`
+// })
+
+let  result = document.querySelector(".result");
+let inc = document.querySelector(".inc");
+let dec = document.querySelector(".dce");
+
+let a = 0;
+
+inc.addEventListener("click",function(){
+    if (a === 5){
+        alert("Limit Exceeded");
+        inc.style.background = "#cc8e35";
+    }else{
+        a++;
+        result.innerHTML = a;
+        
+    }
+});
+
+  let b = 0;
+  dec.addEventListener("click",function(){
+    if ( a <= 1){
+         alert("sorry ..")
+         dec.style.background = "#82589F"
+    }else{
+         a--;
+         result.textContent = a;
+    }
+ });
+
+
+
+
+
+
+
